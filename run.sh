f0f9775f58ab638d3e139082ca91cf472de91d04
Datzu712 <devdatzucl@gmail.com>
2021-11-30 18:27:25 -0600

    dev

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+mode=`cat .env | grep APP_MODE= | sed s/APP_MODE=//`
+
+echo $mode
+
+if [ "$mode" == "development" ]; then
+    echo "Running in development mode"
+    npm run dev
+else
+    echo "Running in production mode"
+    npm run build:start
+fi; 
