d19e300a67af146997ed810f82bcdd643e60d6c5
Datzu712 <devdatzucl@gmail.com>
2022-07-01 00:16:51 +0000

    logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Client from './structures/Client';
 
-new Client({
+const bot = new Client({
     intents: 32767,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
+bot.logger.info('Hello');
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,8 +1,18 @@
 import { Client as djsClient, type ClientOptions } from 'discord.js';
+import { Logger } from './Logger';
+import { resolve } from 'path';
 
 export default class Client extends djsClient {
+    public readonly logger: Logger;
+
     constructor(options: ClientOptions) {
         super(options);
-        console.log('zzzz');
+
+        const debug = process.env.NODE_ENV === 'development' ? true : false;
+        this.logger = new Logger({
+            debugAllowed: debug,
+            folderPath: resolve(__dirname, '../../logs/'),
+            textTemplate: '[{timestamp}] - {level:5} {service:15} {message}'
+        });
     }
 }
